36c2dff5cfeccbf3fcb768b1e3971f2d1765f6d8
Gennadios Fountas <messigeorge138@gmail.com>
2025-01-15 13:49:31 +0200

    Add files via upload

---
+++ testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ingo Boehme on 26.02.23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
